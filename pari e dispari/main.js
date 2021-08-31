7ce07cbbdb6db62890c330b4c30cf242ee2aa9f0
Mikele91 <michele.amoruso91.8@gmail.com>
2021-08-31 17:47:38 +0200

    terminato (manca la verifica per i numeri 1/5)

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,11 +1,53 @@
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-var scelta = prompt("inserisci pari o dispari").toLowerCase();
-while(scelta  != "pari" || scelta != "dispari"){
-    scelta = prompt("Iserisci una scelta valida: PARI O DISPARI").toLowerCase();
+// FUNZIONI
+function numeroRandom(min, max){
+
+    var random = Math.floor(Math.random() * (max-min +1)+min);
+     return random;
+
+
+
 }
 
 
+ function isEven(num1){
+     if((num1 % 2) ==0){
+        return true;
+    }else{
+       return false;
+    }
+ }
+
+
+
+
+
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+var scelta = prompt("inserisci pari o dispari").toLowerCase();
+while(scelta  != "pari" && scelta != "dispari"){
+    scelta = prompt("Iserisci una scelta valida: PARI O DISPARI").toLowerCase();
+}
+var sceltaNumero = parseInt(prompt("inserisci un numero da 1 a 5"));
+// while(sceltaNumero  != range(1, 5) ){
+//     sceltaNumero = parseInt(prompt("inserisci un numero da 1 a 5"));
+// }
+console.log(scelta);
+console.log(sceltaNumero);
 // Generiamo un numero random (sem)pre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
+var numeroUtente = numeroRandom(1, 5);
+var numeroPc = numeroRandom(1, 5);
+
+var somma = numeroUtente + numeroPc;
+
+console.log(somma);
+
+if(isEven(somma) == scelta) {
+    alert("hai vinto")
+
+}else{
+    alert("hai perso")
+}
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
 // Dichiariamo chi ha vinto.
