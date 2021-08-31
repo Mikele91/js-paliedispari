eedf1e52786a912b5bdb376b5d48fe13f008351d
Mikele91 <michele.amoruso91.8@gmail.com>
2021-08-31 17:08:46 +0200

    convalidazione parola

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,5 +1,11 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+var scelta = prompt("inserisci pari o dispari").toLowerCase();
+while(scelta  != "pari" || scelta != "dispari"){
+    scelta = prompt("Iserisci una scelta valida: PARI O DISPARI").toLowerCase();
+}
+
+
+// Generiamo un numero random (sem)pre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
